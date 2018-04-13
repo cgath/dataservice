09ca74c8302964e76010cc2b29df74b02da243ca
Christian Halfdan Gath <CHGA@foss.dk>
2018-04-13 16:51:54 +0200

    Adds .gitignore

--- DataService/Types.cs
+++ DataService/Types.cs
@@ -6,14 +6,21 @@ namespace DataService.Types
     public class SampleSet
     {
         private Guid _id;
-        private List<Segment> _inBase;
+        private List<Segment> _segments;
         private string _data_uri;
         private string _ref_uri;
+        private int[] _idx_vector;
+        private int[] _rep_vector;
+        private int[] _delete_vector;
 
-        public List<Segment> inBase { get => _inBase; set => _inBase = value; }
+        public List<Segment> Segments { get => _segments; set => _segments = value; }
         public Guid ID { get => _id; set => _id = value; }
-        public string dataUri { get => _data_uri; set => _data_uri = value; }
-        public string refUri { get => _ref_uri; set => _ref_uri = value; }
+        public string DataUri { get => _data_uri; set => _data_uri = value; }
+        public string RefUri { get => _ref_uri; set => _ref_uri = value; }
+        
+        public int[] IdxVector { get => _idx_vector; }
+        public int[] RepVector { get => _rep_vector; }
+        public int[] DeleteVector { get => _delete_vector; }
     }
 
     public class Segment
